97b81e461f2363dec0a8dc9ca95716a6027f0f2b
9AY <92072836+D1051237@users.noreply.github.com>
2023-10-30 09:31:19 +0800

    Add files via upload

--- HW02/src/main/java/org/example/Main.java
+++ HW02/src/main/java/org/example/Main.java
@@ -1,14 +1,75 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 class HW02 {
-    int addOne(int x){
-        return x + 1;
+    // Get the BMI of the height and the weight
+    double getbmi(double height, double weight){
+        return weight / ((height/100) * (height/100));
     }
 
-    int add(int x, int y){
-        return x + y;
+    // Get the dayofweek of the specified month and date
+    String daychecker(int month,int day){
+
+        Calendar calendar = Calendar.getInstance();
+        int curr_year = 2021;
+
+        Calendar Date_selected = Calendar.getInstance();
+        Date_selected.set(curr_year, month - 1, day);
+
+        SimpleDateFormat dayformat = new SimpleDateFormat("EEEE");
+        Date date = Date_selected.getTime();
+        String dayofweek =dayformat.format(date);
+        return dayofweek;
     }
 
+    // Get the information of the next day
+    String Checknextday(String daystring){
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            Date date = dateFormat.parse(daystring); // turn input date into Date object
+
+            // Use Calendar class to modify the date
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(date);
+
+            // Add 1 to the Day
+            calendar.add(Calendar.DAY_OF_YEAR, 1);
+
+            // Get modified day
+            Date nextDay = calendar.getTime();
+
+            // Change the format of the day and return the string
+            return dateFormat.format(nextDay);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // Currency class
+    class Currency {
+        int amount;
+        String symbol;
+        public Currency(int a, String s){
+            this.amount = a;
+            this.symbol = s;
+        }
+        public Currency add(Currency other){
+            if(this.symbol.equals(other.symbol)){
+                return new Currency(this.amount + other.amount, this.symbol);
+            }
+            else if(this.symbol.equals("NT")){
+                return new Currency(this.amount + other.amount*30, this.symbol);
+            }
+            else{
+                return new Currency(this.amount + other.amount/30,this.symbol);
+            }
+        }
+    }
 }
+
--- HW02/src/test/java/org/example/HW02Test.java
+++ HW02/src/test/java/org/example/HW02Test.java
@@ -1,29 +1,79 @@
 package org.example;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Currency;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HW02Test {
 
-    @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3, 4})
-    void addOnetest(int x){
+    @DisplayName("BMI Test with CsvSource method")
+    @ParameterizedTest(name = "BMI {2} is correct")
+    @CsvSource({
+            "160, 50, 19.53",  // Test case 1
+            "170, 70, 24.22",  // Test case 2
+            "180, 80, 24.69"   // Test case 3
+    })
+    public void testCalculateBMI(int height, int weight, double expectedBMI) {
         HW02 hw2 = new HW02();
-        assertEquals(x + 1,hw2.addOne(x));
+        assertEquals(expectedBMI, hw2.getbmi(height,weight), 0.01); // 0.01 tolerance for double comparison
     }
-    
+
+    @DisplayName("Day of week test with CsvSource method")
+    @ParameterizedTest(name = "2021年{0}月{1}日 是 {2}")
+    @CsvSource({
+            "1, 4, 星期一",
+            "4, 21, 星期三",
+            "5, 30, 星期日",
+            "7, 8, 星期四",
+            "12, 25, 星期六"
+    })
+    void Checkdate1(int m,int d,String dw){
+        HW02 hw2 = new HW02();
+        assertEquals(dw, hw2.daychecker(m,d));
+    }
+
+    @DisplayName("Day of week Test with CsvFileSource method")
+    @ParameterizedTest(name = "2021年{0}月{1}日 是 {2}")
+    @CsvFileSource(resources = "/DateTestdata.csv", numLinesToSkip = 1)
+    void Checkdate2(int m,int d,String dw){
+        HW02 hw02 = new HW02();
+        assertEquals(dw,hw02.daychecker(m,d));
+    }
+
+    @DisplayName("Currency test with CsvSource method")
     @ParameterizedTest
     @CsvSource({
-            "1,2,3",
-            "0,-1,-1",
-            "1,0,1",
-            "1,-1,0"
+            "100, NT, 100, NT, 200, NT",   // Test case 1
+            "100, US, 3000, NT, 200, US",  // Test case 2
+            "100, NT, 3, US, 190, NT"    // Test case 3
     })
-    void testadd1(int x,int y,int r){
+    void currencytest(int fst_amount, String fst_sym, int sec_amount, String sec_sym, int expectedCurr, String expectedSym){
         HW02 hw2 = new HW02();
-        assertEquals(r,hw2.add(x,y));
+        HW02.Currency Currency1 = hw2.new Currency(fst_amount,fst_sym);
+        HW02.Currency Currency2 = hw2.new Currency(sec_amount,sec_sym);
+        HW02.Currency result = Currency1.add(Currency2);
+
+        assertEquals(expectedCurr, result.amount);
+        assertEquals(expectedSym, result.symbol);
+    }
+
+    @DisplayName("NextDay test with CsvSource method")
+    @ParameterizedTest
+    @CsvSource({
+            "2023-01-01, 2023-01-02",
+            "2023-03-07, 2023-03-08",
+            "2023-05-15, 2023-05-16",
+            "2023-10-21, 2023-10-22",
+            "2023-12-07, 2023-12-08"
+    })
+    void Checknextday(String day,String nextday){
+        HW02 hw02 = new HW02();
+        assertEquals(nextday,hw02.Checknextday(day));
     }
 }
